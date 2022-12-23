3e672e34318c82707b5019d6d86c93fa7c68ad7b
Park youngho <snu910501@naver.com>
2022-12-23 20:16:52 +0900

    login finish

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,11 +1,21 @@
+const LoginService = require('../services/login.service');
+
 class LoginController {
+  loginController = new LoginService();
 
   login = async (req, res, next) => {
     try {
       let email = req.body.email;
       let password = req.body.password;
-    } catch (err) {
 
+      let Token = await this.loginController.login(email, password);
+      return res.status(200).json({
+        result: true,
+        token: Token,
+      })
+    } catch (err) {
+      console.log(err);
+      return res.status(err.status).json({ errorMessage: err.errorMessage })
     }
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const signupRouter = require("./signup.routes");
+const loginRouter = require('./login.routes');
 
-router.use('/signup', signupRouter)
+router.use('/login', loginRouter);
+router.use('/signup', signupRouter);
 
 module.exports = router;
