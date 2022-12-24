5c564cfa599f60be86446725f772c9701fa727ae
Suyoung Kim <110752019+alan-rla@users.noreply.github.com>
2022-12-24 10:51:22 +0900

    env reset

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
-const Sequelize = require('sequelize');
-const User = require('./user');
+const Sequelize = require("sequelize");
+const User = require("./user");
 
-
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 const sequelize = new Sequelize(
@@ -17,8 +16,10 @@ db.sequelize = sequelize;
 
 // 여기에다가 db객체에 모델들을 넣어주세요.
 db.User = User;
+db.Post = Post;
 
 // 여기다가 각 모델의 init함수에 sequelize객체를 연결해주세요.
 User.init(sequelize);
+Post.init(sequelize);
 
 module.exports = db;
