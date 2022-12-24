fc94ee942f0d4513992fc6e1ee2b5db7fa551aac
Suyoung Kim <110752019+alan-rla@users.noreply.github.com>
2022-12-24 18:55:55 +0900

    changed upload vid middleware

--- middlewares/uploadVidToS3.js
+++ middlewares/uploadVidToS3.js
@@ -1,6 +1,3 @@
-// const multer = require("multer");
-// const multerS3 = require("multer-s3");
-// const AWS = require("aws-sdk");
 const { S3 } = require("aws-sdk");
 const fs = require("fs");
 const path = require("path");
