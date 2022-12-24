5effce8487f81e9eae012aea3522c3b57edb39f3
Park youngho <snu910501@naver.com>
2022-12-24 11:20:05 +0900

    upload

--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -4,7 +4,21 @@ const router = express.Router();
 const SignupController = require("../controllers/signup.controller");
 const signupController = new SignupController();
 
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    },
+  }),
+  limits: {
+    fileSize: 5 * 1024 * 1024
+  }
+})
+
 router.post('/', signupController.registerUser);
-router.post('/emailcheck', signupController.checkEmail);
+router.post('/emailcheck', upload.array('image', 1), signupController.checkEmail);
 
 module.exports = router;
