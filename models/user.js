c6dfd7e1ebcf7cec8ce96c36c990be59dfa79ea8
Park youngho <snu910501@naver.com>
2022-12-26 19:27:12 +0900

    hi

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ module.exports = class User extends Sequelize.Model {
           defaultValue: "local",
         },
         snsId: {
-          type: Sequelize.STRING(30),
+          type: Sequelize.INTEGER,
           allowNull: true,
         },
       },
