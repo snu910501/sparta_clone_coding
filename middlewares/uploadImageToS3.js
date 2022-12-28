bcfc3f39e456921ffc7235234a6dc2698d0c3725
Suyoung Kim <110752019+alan-rla@users.noreply.github.com>
2022-12-28 17:34:04 +0900

    added uploaded file deletion in ec2

--- middlewares/uploadImageToS3.js
+++ middlewares/uploadImageToS3.js
@@ -14,28 +14,33 @@ module.exports = uploadVidToS3 = async (image) => {
     const fileStream = fs.createReadStream(file.path);
     // buffer, stream
 
-    return s3.upload({
-      Bucket: 'clone-coding-syk',
-      // 파일명
-      Key: `${file.originalname}`,
-      Body: fileStream,
-    })
+    return s3
+      .upload({
+        Bucket: "clone-coding-syk",
+        // 파일명
+        Key: `${file.originalname}`,
+        Body: fileStream,
+      })
       .promise();
   });
 
-
-  //   fs.rmdir("uploads/", { recursive: true }, (err) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //   });
-
   const result = await Promise.all(promiseList);
 
-  console.log('url', result);
-  result.map(v => {
-    url.push({ location: v.Location, fileName: v.key })
+  console.log("url", result);
+  result.map((v) => {
+    url.push({ location: v.Location, fileName: v.key });
   });
 
-  return url
+  // S3 업로드 후 uploads 폴더 내 파일 삭제
+  fs.readdir("uploads/", (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join("uploads/", file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
+  return url;
 };
